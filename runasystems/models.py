0a0931cbd8fcb132b0ae26ddb72edaa8b6aad87a
Yakubovich <yakuboshka93@mail.ru>
2020-12-06 11:56:21 +0300

    Added name=unique

--- runasystems/models.py
+++ runasystems/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Categories(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     parent = models.ForeignKey(
         'self', blank=True, null=True, on_delete=models.SET_NULL, related_name='children'
     )
